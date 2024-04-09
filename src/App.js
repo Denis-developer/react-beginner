fd0c2cc96f7222ab6c60aecb4ad3d4abef6083d8
Denis <patron.2001den@mail.ru>
2024-04-09 11:18:34 +0700

    Modal completed

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
-import React from 'react'
-
+import React from 'react';
 import './index.scss';
+
+function Modal({ toggleModal, children }) {
+    return (
+        <div className="modal">
+            <svg height="200" viewBox="0 0 200 200" width="200" onClick={toggleModal}>
+                <title />
+                <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+            </svg>
+            <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+            {children}
+        </div>
+    )
+}
 
 function App() {
 
-  const [count, setCount] = React.useState(4);
+    const [modal, setModal] = React.useState(false);
 
-  const countPlus = () => {
-    setCount(count + 1);
-  }
+    const toggleModal = () => {
+        setModal(!modal);
+    }
 
-  const countMinus = () => {
-    setCount(count - 1);
-  }
-
-  return (
-    <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>{count}</h1>
-        <button className="minus" onClick={countMinus}>- Минус</button>
-        <button className="plus" onClick={countPlus}>Плюс +</button>
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <button className="open-modal-btn" onClick={toggleModal}>✨ Открыть окно</button>
+            <div className={`overlay animated ${modal ? "show" : null}`}>
+                <Modal toggleModal={toggleModal}>
+                    {/* <h2>Hello World!!!</h2> */}
+                </Modal>
+            </div>
+        </div >
+    );
 }
 
 export default App;
