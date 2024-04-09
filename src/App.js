5ad31033dbbf17d6744ae60c6ec197dad8f2e7aa
Denis <patron.2001den@mail.ru>
2024-04-09 11:32:36 +0700

    quiz completed

--- src/App.js
+++ src/App.js
@@ -1,36 +1,74 @@
 import React from 'react';
 import './index.scss';
 
-function Modal({ toggleModal, children }) {
+const questions = [
+    {
+        title: 'React - это ... ?',
+        variants: ['библиотека', 'фреймворк', 'приложение'],
+        correct: 0,
+    },
+    {
+        title: 'Компонент - это ... ',
+        variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
+        correct: 1,
+    },
+    {
+        title: 'Что такое JSX?',
+        variants: [
+            'Это простой HTML',
+            'Это функция',
+            'Это тот же HTML, но с возможностью выполнять JS-код',
+        ],
+        correct: 2,
+    },
+];
+
+function Result({ correct }) {
     return (
-        <div className="modal">
-            <svg height="200" viewBox="0 0 200 200" width="200" onClick={toggleModal}>
-                <title />
-                <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-            </svg>
-            <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
-            {children}
+        <div className="result">
+            <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
+            <h2>Вы отгадали {correct} ответа из {questions.length}</h2>
+            <a href='/'>
+                Попробовать снова
+            </a>
         </div>
-    )
+    );
+}
+
+function Game({ question, step, onClickAnswer }) {
+
+    const percent = Math.round((step / questions.length) * 100);
+
+    return (
+        <>
+            <div className="progress">
+                <div style={{ width: `${percent}%` }} className="progress__inner"></div>
+            </div>
+            <h1>{question.title}</h1>
+            <ul>
+                {question.variants.map((item, i) => <li key={i} onClick={() => onClickAnswer(i)}>{item}</li>)}
+            </ul>
+        </>
+    );
 }
 
 function App() {
 
-    const [modal, setModal] = React.useState(false);
+    const [step, setStep] = React.useState(0);
+    const [correct, setCorrect] = React.useState(0);
+    const question = questions[step];
 
-    const toggleModal = () => {
-        setModal(!modal);
+    const onClickAnswer = (index) => {
+        if (index === question.correct) {
+            setCorrect(correct + 1);
+        }
+        setStep(step + 1);
     }
 
     return (
         <div className="App">
-            <button className="open-modal-btn" onClick={toggleModal}>✨ Открыть окно</button>
-            <div className={`overlay animated ${modal ? "show" : null}`}>
-                <Modal toggleModal={toggleModal}>
-                    {/* <h2>Hello World!!!</h2> */}
-                </Modal>
-            </div>
-        </div >
+            {step === questions.length ? <Result correct={correct} /> : <Game question={question} step={step} onClickAnswer={onClickAnswer} />}
+        </div>
     );
 }
 
